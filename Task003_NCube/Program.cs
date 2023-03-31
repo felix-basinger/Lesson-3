da4439bd05aa99be723df4f090c4ef1e20c20f6a
Basinger <fel_simon@mail.ru>
2023-04-01 02:46:44 +0300

    Added Task003_NCube

---
+++ Task003_NCube/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Main();
+ 
+void Main()
+{
+    bool Work = true;
+    while(Work)
+    {
+        System.Console.Write("Are you ready? Enter y/n: ");
+        string a = System.Console.ReadLine();
+        switch(a)
+        {
+            case "y":
+            NumCube();
+            break;
+
+            case "n":
+            Work = false;
+            break;
+        }
+    }
+}
+
+int Enter()
+{
+    Console.Write("Enter the number N: ");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+void NumCube()
+{   
+    int N = Enter();
+    for(int i = 1; i <= N; i ++)
+    {
+        Console.Write(Math.Pow(i, 3));
+        Console.Write(", ");
+    }
+}
+ 
